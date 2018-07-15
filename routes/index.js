4ca3f56339dcec32654aa759dec131524f306cd6
Muhammad Adil <adilsaeed31@gmail.com>
2018-07-15 17:40:34 +0400

    Added logo name

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,8 @@ const { BG_ARR } = require('../config');
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', {
-    pageTitle: 'aDil world | Full Stack Developer Web/Mobile/Desktop & Technical Lead',
-    logo: 'aDil.world',
+    pageTitle: 'M.@dil | Full Stack Developer Web/Mobile/Desktop & Technical Lead',
+    logo: 'M.@dil',
     background: BG_ARR[Math.floor(Math.random() * BG_ARR.length)],
     title: 'Welcome',
     subtitle: 'Your visit is valueable to me'
