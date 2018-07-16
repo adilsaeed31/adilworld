633b7ec01d690ac392dfe71c09745a477a5551b6
Muhammad Adil <adilsaeed31@gmail.com>
2018-07-16 18:00:40 +0400

    Change Logo

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,11 @@ const { BG_ARR } = require('../config');
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', {
-    pageTitle: 'M.@dil | Full Stack Developer Web/Mobile/Desktop & Technical Lead',
-    logo: 'M.@dil',
+    pageTitle: '@dil | Full Stack Developer Web/Mobile/Desktop & Technical Lead',
+    logo: '@dil',
     background: BG_ARR[Math.floor(Math.random() * BG_ARR.length)],
-    title: 'Welcome',
-    subtitle: 'Your visit is valueable to me'
+    title: 'Hi, Welcome, Your visit is valueable to me.',
+    subtitle: 'I am, Fullstack developer Web/Mobile/Desktop & Technical Lead.'
   });
 });
 
