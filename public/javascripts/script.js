a4c12c50169728822033b880170088e282a078e2
Muhammad Adil <adil.1063@awok.ae>
2018-12-17 17:52:55 +0400

    fix typo

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,11 +1,11 @@
-const BG_ARR = ['link', 'primary', 'info', 'danger', 'warning', 'success', 'dark'];
+const BG_ARR = ['link', 'primary', 'info', 'danger', 'warning', 'success', 'dark']
 setInterval(() => {
-  let doc = document.getElementById('app');
-  let hiBtn = document.getElementById('hiBtn')
-  let background = BG_ARR[Math.floor(Math.random() * BG_ARR.length)];
-  doc.classList.remove(doc.classList[2]);
-  doc.classList.add(`is-${background}`);
+    let doc = document.getElementById('app')
+    let hiBtn = document.getElementById('hiBtn')
+    let background = BG_ARR[Math.floor(Math.random() * BG_ARR.length)]
+    doc.classList.remove(doc.classList[2])
+    doc.classList.add(`is-${background}`)
 
-  hiBtn.classList.remove(hiBtn.classList[2]);
-  hiBtn.classList.add(`is-${background}`);
-}, 5000);
+    hiBtn.classList.remove(hiBtn.classList[2]);
+    hiBtn.classList.add(`is-${background}`)
+}, 5000)
