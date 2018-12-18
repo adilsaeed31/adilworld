f1a6595b0fae33ed1411f92009820021532f9a97
Muhammad Adil <adil.1063@awok.ae>
2018-12-18 11:41:37 +0400

    Added mobile btn click for navigation

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,11 +1,35 @@
-const BG_ARR = ['link', 'primary', 'info', 'danger', 'warning', 'success', 'dark']
+const BG_ARR = [
+  'link',
+  'primary',
+  'info',
+  'danger',
+  'warning',
+  'success',
+  'dark'
+]
 setInterval(() => {
-    let doc = document.getElementById('app')
-    let hiBtn = document.getElementById('hiBtn')
-    let background = BG_ARR[Math.floor(Math.random() * BG_ARR.length)]
-    doc.classList.remove(doc.classList[2])
-    doc.classList.add(`is-${background}`)
+  let doc = document.getElementById('app')
+  let hiBtn = document.getElementById('hiBtn')
+  let background = BG_ARR[Math.floor(Math.random() * BG_ARR.length)]
+  doc.classList.remove(doc.classList[2])
+  doc.classList.add(`is-${background}`)
 
-    hiBtn.classList.remove(hiBtn.classList[2]);
-    hiBtn.classList.add(`is-${background}`)
+  hiBtn.classList.remove(hiBtn.classList[2])
+  hiBtn.classList.add(`is-${background}`)
 }, 5000)
+
+function handleMobileNav(obj) {
+  if (obj.className.indexOf('is-active') > 0) {
+    obj.removeAttribute('class')
+    obj.setAttribute('class', 'navbar-burger burger')
+    let navBar = document.getElementById('adilHeroNavBar')
+    navBar.removeAttribute('class')
+    navBar.setAttribute('class', 'navbar-menu')
+  } else {
+    obj.removeAttribute('class')
+    obj.setAttribute('class', 'navbar-burger burger is-active')
+    let navBar = document.getElementById('adilHeroNavBar')
+    navBar.removeAttribute('class')
+    navBar.setAttribute('class', 'navbar-menu is-active')
+  }
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
-let express = require('express')
-let router = express.Router()
+const express = require('express')
+const router = express.Router()
 const { BG_ARR } = require('../config')
 
 /* GET home page. */
