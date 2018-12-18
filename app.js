53766ba5011e1a9492bae87c3051dd827428ff97
Muhammad Adil <adil.1063@awok.ae>
2018-12-18 10:23:47 +0400

    Add prettier and eslint and jest

--- app.js
+++ app.js
@@ -1,50 +1,50 @@
-let createError = require('http-errors');
-let express = require('express');
-let path = require('path');
-let cookieParser = require('cookie-parser');
-let logger = require('morgan');
-let sassMiddleware = require('node-sass-middleware');
+const createError = require('http-errors')
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const sassMiddleware = require('node-sass-middleware')
 
-let indexRouter = require('./routes/index');
+const indexRouter = require('./src/routes/index')
 
-let app = express();
+const app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 
-app.use(logger('dev'));
+app.use(logger('dev'))
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
-app.use(cookieParser());
+app.use(cookieParser())
 
 app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true,
-  sourceMap: true
-}));
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
+    indentedSyntax: true,
+    sourceMap: true
+}))
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter);
+app.use('/', indexRouter)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use(function (req, res, next) {
+    next(createError(404))
+})
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message
+    res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+    // render the error page
+    res.status(err.status || 500)
+    res.render('error')
+})
 
-module.exports = app;
+module.exports = app
---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+let express = require('express')
+let router = express.Router()
+const { BG_ARR } = require('../config')
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', {
+    pageTitle:
+      '@dil | Full Stack Developer Web/Mobile/Desktop & Technical Lead',
+    logo: '@dil',
+    background: BG_ARR[Math.floor(Math.random() * BG_ARR.length)],
+    title: 'Hi, Welcome, Your visit is valuable to me.',
+    subtitle:
+      'I am a Full Stack developer Web, Mobile & Desktop and Technical Lead of your next start up.'
+  })
+})
+
+module.exports = router
