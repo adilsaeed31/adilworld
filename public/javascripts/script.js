e6632d699bb9e124c9b178db88ff70864ff63ae7
Muhammad Adil <adilsaeed31@gmail.com>
2019-10-01 11:30:17 +0300

    Added background animation

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -16,13 +16,13 @@ function handleMobileNav(obj) {
   }
 }
 
-// setInterval(() => {
-let doc = document.getElementById('app')
-let hiBtn = document.getElementById('hiBtn')
-let background = BG_ARR[Math.floor(Math.random() * BG_ARR.length)]
-doc.classList.remove(doc.classList[2])
-doc.classList.add(`is-${background}`)
+setInterval(() => {
+  let doc = document.getElementById('app')
+  let hiBtn = document.getElementById('hiBtn')
+  let background = BG_ARR[Math.floor(Math.random() * BG_ARR.length)]
+  doc.classList.remove(doc.classList[2])
+  doc.classList.add(`is-${background}`)
 
-hiBtn.classList.remove(hiBtn.classList[2])
-hiBtn.classList.add(`is-${background}`)
-// }, 5000)
+  hiBtn.classList.remove(hiBtn.classList[2])
+  hiBtn.classList.add(`is-${background}`)
+}, 5000)
