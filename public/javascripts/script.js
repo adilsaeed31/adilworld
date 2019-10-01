92fa71e261d2bcf21921d7c254228ec472d06003
Adil <adil@Mohds-iMac.hotspot>
2019-10-01 11:14:56 +0300

    Added background animation

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,22 +1,4 @@
-const BG_ARR = [
-  'link',
-  'primary',
-  'info',
-  'danger',
-  'warning',
-  'success',
-  'dark'
-]
-setInterval(() => {
-  let doc = document.getElementById('app')
-  let hiBtn = document.getElementById('hiBtn')
-  let background = BG_ARR[Math.floor(Math.random() * BG_ARR.length)]
-  doc.classList.remove(doc.classList[2])
-  doc.classList.add(`is-${background}`)
-
-  hiBtn.classList.remove(hiBtn.classList[2])
-  hiBtn.classList.add(`is-${background}`)
-}, 5000)
+const { BG_ARR } = require('../../src/config/index')
 
 function handleMobileNav(obj) {
   if (obj.className.indexOf('is-active') > 0) {
@@ -33,3 +15,14 @@ function handleMobileNav(obj) {
     navBar.setAttribute('class', 'navbar-menu is-active')
   }
 }
+
+// setInterval(() => {
+let doc = document.getElementById('app')
+let hiBtn = document.getElementById('hiBtn')
+let background = BG_ARR[Math.floor(Math.random() * BG_ARR.length)]
+doc.classList.remove(doc.classList[2])
+doc.classList.add(`is-${background}`)
+
+hiBtn.classList.remove(hiBtn.classList[2])
+hiBtn.classList.add(`is-${background}`)
+// }, 5000)
