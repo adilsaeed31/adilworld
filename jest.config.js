aa3b0381020e89ab6928bd152bf53024c743effd
Muhammad Adil <adilsaeed31@gmail.com>
2019-11-11 12:40:34 +0300

    changes

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-    collectCoverage: true,
-    collectCoverageFrom: [
-        "src/**/*.js",
-        "!**/node_modules/**"
-    ],
-    coverageDirectory: "coverage"
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.js', '!**/node_modules/**'],
+  coverageDirectory: 'coverage'
 }
