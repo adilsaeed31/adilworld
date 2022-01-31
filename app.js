25cf602bcb8548fec640b147a996130a0901a9da
Muhammad Adil <masaeed@phcc.gov.qa>
2022-01-31 13:36:19 +0300

    fix the bg issue

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
-const sassMiddleware = require('node-sass-middleware')
 
 const indexRouter = require('./src/routes/index')
 
@@ -19,15 +18,6 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use(cookieParser())
-
-app.use(
-  sassMiddleware({
-    src: path.join(__dirname, 'public'),
-    dest: path.join(__dirname, 'public'),
-    indentedSyntax: true,
-    sourceMap: true
-  })
-)
 
 app.use(express.static(path.join(__dirname, 'public')))
 
